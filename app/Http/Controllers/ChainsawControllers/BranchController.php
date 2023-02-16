81981f5842ef5eaeab1882f93ad5da4f922ac540
EccoEdd <ceduardoemiliano@gmail.com>
2023-02-15 23:09:24 -0600

    WEB anserws instead of API

---
+++ app/Http/Controllers/ChainsawControllers/BranchController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\ChainsawControllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class BranchController extends Controller
+{
+    public function createBranch(Request $request){
+        $validate = $request->validate([
+            'name' => 'required|max:45|unique:branches',
+            'location' => 'required|unique:branches'
+        ],[
+            'name' => [
+                'required' => 'You need a name for your branch',
+                'max'      => 'You only have 45 characters long',
+                'unique'   => 'This branch already exists'
+            ],
+            'location' => [
+                'required' => 'You need a location for this branch',
+                'unique'   => 'You can\'t have two or more branches in the same spot'
+            ]
+        ]);
+        return 'Kobeni my beloved';
+        if(!$validate)
+            return $request;
+        return $request;
+    }
+    public function readBranches(){
+
+    }
+    public function updateBranch(Request $request, int $id){
+
+    }
+    public function deleteBranch(int $id){
+
+    }
+}
---
+++ database/migrations/2023_02_15_193520_create_characters_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 15)
+                ->nullable(false);
+            $table->string('l_name', 15);
+            $table->string('type', 20);
+            $table->boolean('alive')
+                ->default(true);
+            $table->unsignedTinyInteger('age')
+                ->nullable(false);
+            $table->foreignId('team_id')
+                ->references('id')
+                ->on('teams')
+                ->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characters');
+    }
+};
